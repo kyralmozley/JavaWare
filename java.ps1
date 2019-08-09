8ebee3e0d57cdbc73c13bf41509561bd38243ab8
Kyra <kyralmozley@outlook.com>
2019-08-09 15:26:23 +0100

    Add files via upload

---
+++ java.ps1
@@ -0,0 +1,3 @@
+﻿ cd ..\..\..\..\..\..\..\
+ $java="C:\Program Files\Java\jdk-12.0.1\bin\java.exe"
+ Start-Process -FilePath $java -ArgumentList '--module-path "C:\Users\Admin\Desktop\JavaWare\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls -jar Users\Admin\Desktop\JavaWare\JavaWare.jar'
